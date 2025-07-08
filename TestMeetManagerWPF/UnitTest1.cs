60b85825c2363a781c2b9784188de0cdc263c043
Jiří Strnadel <Juzba88@gmail.com>
2025-07-08 13:38:07 +0200

    Update from home

---
+++ TestMeetManagerWPF/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using 
+
+
+namespace TestMeetManagerWPF
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
