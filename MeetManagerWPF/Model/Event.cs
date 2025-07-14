4786982661f080b9c9306211339a9b18e82aebd8
Jiří Strnadel <Juzba88@gmail.com>
2025-07-15 00:07:52 +0200

    Update event create page

--- MeetManagerWPF/Model/Event.cs
+++ MeetManagerWPF/Model/Event.cs
@@ -12,7 +12,7 @@ namespace MeetManagerWPF.Model
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime EndDate { get; set; } 
 
 
         // EVENT TYPE //
--- MeetManagerWPF/ViewModel/Manager/CreateEventViewModel.cs
+++ MeetManagerWPF/ViewModel/Manager/CreateEventViewModel.cs
@@ -3,11 +3,13 @@ using CommunityToolkit.Mvvm.Input;
 using MeetManagerWPF.Model;
 using MeetManagerWPF.Services;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace MeetManagerWPF.ViewModel.Manager;
 
 public partial class CreateEventViewModel : ObservableObject
 {
+
     private readonly IDataService _dataService;
 
 
@@ -16,18 +18,26 @@ public partial class CreateEventViewModel : ObservableObject
     {
         _dataService = dataService;
         GetEventTypeListCommand.Execute(null);
-        GetRoomListCommand.Execute(null);
     }
 
+  
 
 
-    // COMBOBOX EVENT TYPE //
+
+    // COMBOBOX EVENTTYPES AND ROOMS LISTS FROM DB//
     [RelayCommand]
     private async Task GetEventTypeList()
     {
-        var data = await _dataService.GetEventTypeList();
-        EventTypeList = new ObservableCollection<EventType>(data);
+        var eventTypes = await _dataService.GetEventTypeList();
+        EventTypeList = new ObservableCollection<EventType>(eventTypes);
+
+        var roomsList = await _dataService.GetRoomList();
+        RoomList = new ObservableCollection<Room>(roomsList);
     }
+
+
+
+    // COMBOBOX EVENTTYPE AND ROOMS
 
     [ObservableProperty]
     private ObservableCollection<EventType> eventTypeList = [];
@@ -35,21 +45,11 @@ public partial class CreateEventViewModel : ObservableObject
     [ObservableProperty]
     private EventType? selectedEventType;
 
-
-
-    // COMBOBOX ROOM //
-    [RelayCommand]
-    private async Task GetRoomList()
-    {
-        var data = await _dataService.GetRoomList();
-        RoomList = new ObservableCollection<Room>(data);
-    }
-
     [ObservableProperty]
     private ObservableCollection<Room> roomList = [];
 
     [ObservableProperty]
-    private EventType? selectedRoom;
+    private Room? selectedRoom;
 
 
 
@@ -63,10 +63,10 @@ public partial class CreateEventViewModel : ObservableObject
     private string? name;
 
     [ObservableProperty]
-    private DateTime startEvent;
+    private DateTime startEvent = DateTime.Now.AddDays(7);
 
     [ObservableProperty]
-    private DateTime endEvent;
+    private DateTime endEvent = DateTime.Now.AddDays(8);
 
     [ObservableProperty]
     private string? description;
@@ -77,13 +77,13 @@ public partial class CreateEventViewModel : ObservableObject
     [RelayCommand]
     public async Task CreateEvent()
     {
-        if (Name == null || Description == null || SelectedEventType == null)
+        if (Name == null || Description == null || SelectedEventType == null || SelectedRoom == null)
         {
             ErrorMessage = "Chybí údaje!";
             return;
         }
 
-        var newEvent = new Event() { Name = Name, Description = Description, EndDate = EndEvent, StartDate = StartEvent, EventTypeId = SelectedEventType.Id, RoomID = 0 };
+        var newEvent = new Event() { Name = Name, Description = Description, EndDate = EndEvent, StartDate = StartEvent, EventTypeId = SelectedEventType.Id, RoomID = SelectedRoom.ID };
         await _dataService.AddEvent(newEvent);
 
     }
