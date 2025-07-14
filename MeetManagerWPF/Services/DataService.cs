6084fd6bdf62cf680a403d8fa220888ce40a72dd
Jiří Strnadel <Juzba88@gmail.com>
2025-07-14 19:04:52 +0200

    createevent viewmodel changes

--- MeetManagerWPF/Services/DataService.cs
+++ MeetManagerWPF/Services/DataService.cs
@@ -7,9 +7,12 @@ namespace MeetManagerWPF.Services
     public interface IDataService
     {
         Task AddUser(User user);
+        Task AddEvent(Event newEvent);
         Task DeleteUser(User user);
         Task<User?> GetUser(string email);
-        Task<IEnumerable<User>> GetUsersList();
+        Task<ICollection<User>> GetUsersList();
+        Task<ICollection<EventType>> GetEventTypeList();
+        Task<ICollection<Room>> GetRoomList();
         Task UpdateUsersList();
     }
 
@@ -27,6 +30,13 @@ namespace MeetManagerWPF.Services
             await _db.SaveChangesAsync();
         }
 
+        // ADD EVENT //
+        public async Task AddEvent(Event newEwent)
+        {
+            await _db.Events.AddAsync(newEwent);
+            await _db.SaveChangesAsync();
+        }
+
         // DELETE USER //
         public async Task DeleteUser(User user)
         {
@@ -36,24 +46,19 @@ namespace MeetManagerWPF.Services
 
 
         // GET USER //
-        public async Task<User?> GetUser(string email)
-        {
-            return await _db.Users.Include(p => p.Role).FirstOrDefaultAsync(p => p.Email == email);
-        }
-
+        public async Task<User?> GetUser(string email) => await _db.Users.Include(p => p.Role).FirstOrDefaultAsync(p => p.Email == email);
 
         // GET USERS LIST //
-        public async Task<IEnumerable<User>> GetUsersList()
-        {
-            return await _db.Users.Include(p => p.Role).ToListAsync();
-        }
+        public async Task<ICollection<User>> GetUsersList() => await _db.Users.Include(p => p.Role).ToListAsync();
 
+        // GET EVENT TYPE LIST //
+        public async Task<ICollection<EventType>> GetEventTypeList() => await _db.EventTypes.ToListAsync();
 
-        // UPDATE USERS LIST //
-        public async Task UpdateUsersList()
-        {
-            await _db.SaveChangesAsync();
-        }
+        // GET EVENT TYPE LIST //
+        public async Task<ICollection<Room>> GetRoomList() => await _db.Rooms.ToListAsync();
+
+        // SAVE USERS LIST //
+        public async Task UpdateUsersList() => await _db.SaveChangesAsync();
 
 
     }
--- MeetManagerWPF/ViewModel/Manager/CreateEventViewModel.cs
+++ MeetManagerWPF/ViewModel/Manager/CreateEventViewModel.cs
@@ -1,13 +1,111 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using MeetManagerWPF.Model;
+using MeetManagerWPF.Services;
+using System.Collections.ObjectModel;
 
-namespace MeetManagerWPF.ViewModel.Manager
+namespace MeetManagerWPF.ViewModel.Manager;
+
+public partial class CreateEventViewModel : ObservableObject
 {
-    public class CreateEventViewModel: ObservableObject
+    private readonly IDataService _dataService;
+
+
+
+    public CreateEventViewModel(IDataService dataService)
     {
+        _dataService = dataService;
+        GetEventTypeListCommand.Execute(null);
+        GetRoomListCommand.Execute(null);
     }
+
+
+
+    // COMBOBOX EVENT TYPE //
+    [RelayCommand]
+    private async Task GetEventTypeList()
+    {
+        var data = await _dataService.GetEventTypeList();
+        EventTypeList = new ObservableCollection<EventType>(data);
+    }
+
+    [ObservableProperty]
+    private ObservableCollection<EventType> eventTypeList = [];
+
+    [ObservableProperty]
+    private EventType? selectedEventType;
+
+
+
+    // COMBOBOX ROOM //
+    [RelayCommand]
+    private async Task GetRoomList()
+    {
+        var data = await _dataService.GetRoomList();
+        RoomList = new ObservableCollection<Room>(data);
+    }
+
+    [ObservableProperty]
+    private ObservableCollection<Room> roomList = [];
+
+    [ObservableProperty]
+    private EventType? selectedRoom;
+
+
+
+
+
+
+    [ObservableProperty]
+    private string? errorMessage;
+
+    [ObservableProperty]
+    private string? name;
+
+    [ObservableProperty]
+    private DateTime startEvent;
+
+    [ObservableProperty]
+    private DateTime endEvent;
+
+    [ObservableProperty]
+    private string? description;
+
+
+
+
+    [RelayCommand]
+    public async Task CreateEvent()
+    {
+        if (Name == null || Description == null || SelectedEventType == null)
+        {
+            ErrorMessage = "Chybí údaje!";
+            return;
+        }
+
+        var newEvent = new Event() { Name = Name, Description = Description, EndDate = EndEvent, StartDate = StartEvent, EventTypeId = SelectedEventType.Id, RoomID = 0 };
+        await _dataService.AddEvent(newEvent);
+
+    }
+
+
+    [RelayCommand]
+    public void DeleteEvent()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
